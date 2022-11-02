73c1a061cef353ee11e778ee502a595094673bfa
mikolvj <mikolajborowskiii@gmail.com>
2022-11-02 18:09:53 +0100

    logic finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
+import { useEffect, useState } from 'react';
 import classes from './App.module.scss';
 import CurrencyInput from './CurrencyInput';
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ec0ffd287bmsh5987d6c045af909p16fb41jsn20b0d2d48342',
+		'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com',
+	},
+};
+
 function App() {
+	const [currenciesList, setCurrenciesList] = useState([]);
+	const [fromCurrency, setFromCurrency] = useState('EUR');
+	const [toCurrency, setToCurrency] = useState('USD');
+	const [amount, setAmount] = useState(1);
+	const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+	const [exchangeRate, setExchangeRate] = useState();
+
+	let fromAmount, toAmount;
+
+	if (amountInFromCurrency) {
+		fromAmount = amount;
+		toAmount = Math.round(amount * exchangeRate * 100) / 100;
+	} else {
+		toAmount = amount;
+		fromAmount = Math.round((amount / exchangeRate) * 100) / 100;
+	}
+
+	useEffect(() => {
+		fetch('https://currency-converter5.p.rapidapi.com/currency/list', options)
+			.then((response) => response.json())
+			.then((data) => setCurrenciesList([...Object.keys(data.currencies)]))
+			.catch((err) => console.error(err));
+	}, []);
+
+	useEffect(() => {
+		fetch(
+			`https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=${fromCurrency}&to=${toCurrency}`,
+			options
+		)
+			.then((response) => response.json())
+			.then((data) => setExchangeRate(data.rates[toCurrency].rate))
+			.catch((err) => console.error(err));
+	}, [fromCurrency, toCurrency]);
+
+	const fromCurrencyChangeHandler = (e) => {
+		setAmount(e.target.value);
+		setAmountInFromCurrency(true);
+	};
+	const toCurrencyChangeHandler = (e) => {
+		setAmount(e.target.value);
+		setAmountInFromCurrency(false);
+	};
+
 	return (
 		<div className={classes.wrapper}>
 			<h1>Converter</h1>
-			<CurrencyInput />
-			<p>=</p>
-			<CurrencyInput />
+			<CurrencyInput
+				currenciesList={currenciesList}
+				selectedCurrency={fromCurrency}
+				onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+				onChangeAmount={fromCurrencyChangeHandler}
+				amount={fromAmount}
+			/>
+			<CurrencyInput
+				currenciesList={currenciesList}
+				selectedCurrency={toCurrency}
+				onChangeCurrency={(e) => setToCurrency(e.target.value)}
+				onChangeAmount={toCurrencyChangeHandler}
+				amount={toAmount}
+			/>
 		</div>
 	);
 }
--- src/CurrencyInput.js
+++ src/CurrencyInput.js
@@ -1,11 +1,22 @@
 import classes from './CurrencyInput.module.scss';
 
-const CurrencyInput = () => {
+const CurrencyInput = ({
+	currenciesList,
+	selectedCurrency,
+	onChangeCurrency,
+	onChangeAmount,
+	amount
+}) => {
+	const sortedCurrenciesList = currenciesList.sort();
 	return (
 		<div className={classes.wrapper}>
-			<input type='number' />
-			<select>
-				<option value='EUR'>EUR</option>
+			<input type='number' min="0" value={amount || ''} onChange={onChangeAmount} />
+			<select value={selectedCurrency} onChange={onChangeCurrency}>
+				{sortedCurrenciesList.map((item) => (
+					<option key={item} value={item}>
+						{item}
+					</option>
+				))}
 			</select>
 		</div>
 	);
